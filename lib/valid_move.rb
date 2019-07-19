3a31b20733e906d00950655e80e7bba8da300a2f
Jordan Nelson <jordannelson1.2014@u.northwestern.edu>
2019-07-19 19:40:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,16 @@
 # code your #valid_move? method here
-
+def some_new_fabulous_method
+    if position_taken?(board, ind)
+      True
+    else
+    end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, ind)
+  if board[ind] == " " || board[ind] == "" || board[ind] == nil
+    return false
+  else
+    return true
+  end
+end
